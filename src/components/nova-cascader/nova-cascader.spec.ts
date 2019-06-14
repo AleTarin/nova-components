e6ca9b860c6ec5766e1f575984e92335ddac265e
alejandroroiz <alejandro.roiz.walss@gmail.com>
2019-06-14 13:33:12 -0500

    [wip] testing components

---
+++ src/components/nova-cascader/nova-cascader.spec.ts
@@ -0,0 +1,5 @@
+import { E2EElement, E2EPage, newE2EPage } from '@stencil/core/testing';
+
+describe('nova-cascader e2e', () => {
+    //it should render component with props
+  });
--- src/utils/utils.spec.ts
+++ src/utils/utils.spec.ts
@@ -1,12 +1,21 @@
-import { newSpecPage } from '@stencil/core/testing';
-import { NovaCascader } from '../components/nova-cascader/nova-cascader';
+import { format } from './utils';
 
-describe('Cascader', () => {
-  it('should render cascader component', async () => {
-    const page = await newSpecPage({
-      components: [NovaCascader],
-      html: `<nova-cascader></nova-cascader>`,
-    });
-    expect(page.root).toEqualHtml(`<nova-cascader></nova-cascader>`);
+describe('format', () => {
+  it('returns empty string for no names defined', () => {
+    expect(format(undefined, undefined, undefined)).toEqual('');
+  });
+
+  it('formats just first names', () => {
+    expect(format('Joseph', undefined, undefined)).toEqual('Joseph');
+  });
+
+  it('formats first and last names', () => {
+    expect(format('Joseph', undefined, 'Publique')).toEqual('Joseph Publique');
+  });
+
+  it('formats first, middle and last names', () => {
+    expect(format('Joseph', 'Quincy', 'Publique')).toEqual(
+      'Joseph Quincy Publique'
+    );
   });
 });
