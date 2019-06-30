97c134023ade81216045675a3186805990e1f4fc
Alejandro De la Cruz Tarin <ale.tarin10@gmail.com>
2019-06-29 20:04:37 -0500

    [calendar]-change months and refactor

--- src/components/nova-calendar/nova-calendar.tsx
+++ src/components/nova-calendar/nova-calendar.tsx
@@ -1,4 +1,5 @@
-import { Component, Prop, State, Element, h } from '@stencil/core';
+import { Component, Prop, State, Element, h, Watch } from '@stencil/core';
+import { range } from '../../utils/utils';
 import moment from 'moment';
 
 @Component({
@@ -17,17 +18,32 @@ export class NovaCalendar {
   @Prop() mode:string="month";
   @Prop() activeMonth = Number(moment().format('M'));
   @Prop() activeYear = Number(moment().format('YYYY'));
+  
   @Prop() calendar : any[] = [];
 
   @Prop() card: boolean = false;
 
   // https://momentjs.com/docs/#/displaying/format/
   @State() now: any = moment();
+  @State() calendarEvents: {};
+  @State() months: string[];
+  @State() years: number[]
+  @State() days: string[];
 
   @Element() public host: HTMLElement;
 
-  nowPrevMonth(){
-    this.now = moment(this.now).subtract(1, 'months')
+  nowChangeMonth(month){
+    console.log(this.activeMonth, month)
+    if(month < this.activeMonth) {
+      this.now = moment(this.now).subtract(1, 'months');
+    } 
+    else if (month > this.activeMonth) {
+      this.now = moment(this.now).add(1, 'months')
+    } else {
+      return;
+    }
+    this.activeMonth = Number(this.now.format('M')) - 1;
+    console.log(this.activeMonth)
     this.fillCalendar();
   }
   nowNextMonth(){
@@ -36,101 +52,48 @@ export class NovaCalendar {
   }
 
   nowSetYear(event){
-    this.now = moment(this.now).year(event.target.value);
+    this.activeYear = event.target.value;
+    this.now = moment(this.now).year(this.activeYear);
     this.fillCalendar();
   }
+  
   nowSetMonth(event){
-    this.now = moment(this.now).month(event.target.value);
+    this.activeMonth = Number(event.target.value);
+    this.now = moment(this.now).month(this.activeMonth);
     this.fillCalendar();
   }
 
-  cellContent(cell){
-    var x;
-    var nxt = 1;
-    {if (cell === 98)
-      x = 'prv'
-    else if (cell === 99){
-      x = nxt;
-      nxt++;
-    }
-    else
-      x = cell;
-    }
-    return x;
-  }
-
-  fillCalendar(){
-    
-    console.log("Date: " + this.now.format("YYYY MM DD"))
-
+  fillCalendar() {
     this.calendar = [];
-    var day = 1;
-    var firstDayOfMonth = this.now.startOf('month').format('d')
-    var lastDayOfPrevMonth = Number(moment(this.now).subtract(1, 'M').endOf("month").format('D'));
-    var dayPrevMonth = lastDayOfPrevMonth - firstDayOfMonth +1;
-    var dayNextMonth = 1;
-    var arr = [];
-
-    for(var i = 0; i < 6; i++){
-      for(var j = 0; j < 7; j++){
-        if (i === 0 && j < firstDayOfMonth){
-          arr.push(dayPrevMonth);
-          dayPrevMonth++;
-        }
-        else if (day >this.now.daysInMonth()){
-          if(j<7){
-            arr.push(dayNextMonth);
-            dayNextMonth++;
-          }
-          else
-            break;
-        }
-        else {
-          arr.push(day); 
-          day++;
-        }
-      }
-      this.calendar.push(arr);
-      arr=[];
+    const startDay = this.now.clone().startOf('month').startOf('week');
+    const endDay = this.now.clone().endOf('month').endOf('week');
+    let date = startDay.clone().subtract(1, 'day');
+    
+    while (date.isBefore(endDay, 'day')) {
+        this.calendar.push(Array(7).fill(0).map(() => {
+              const d = date.add(1, 'day').clone();
+              return {day: d.format('D'), month: Number(d.format('M')) - 1}
+            }))
     }
-    
-  }
-
-  // Function that changes the HTML Class to those days that are not in the selected month
-  shadowDaysNotAvailable() {
-    var firstDayOfMonth = Number(this.now.startOf('month').format('d'))
-    var lastDayOfMonth = Number(this.now.endOf('month').format('DD'))
-    var listOfDays = this.host.shadowRoot.querySelectorAll('div.calendar__week')
-    var firstWeek = listOfDays[1]
-    var lastWeek = listOfDays[listOfDays.length - 1]
-    
-    // Loop through the week to get each day
-    firstWeek.childNodes.forEach(day => {
-      // If the day is bigger than the start of the month, it is because it belongs to the earlier month
-      if (Number(day.textContent) > firstDayOfMonth) {
-        var inactiveDay = day.childNodes[0].firstChild
-        inactiveDay.parentElement.className = 'calendar__number__inactive'   
-      }
-    })
-
-    // Loop through the week to get each day
-    lastWeek.childNodes.forEach(day => {
-      // If the day is smaller than the end of the month, it belongs to the next month
-      if (Number(day.textContent) < lastDayOfMonth) {
-        var inactiveDay = day.childNodes[0].firstChild
-        inactiveDay.parentElement.className = 'calendar__number__inactive'
-      }
-    })
-    
+    console.log('calendar:', this.calendar)
   }
 
   componentWillLoad(){
-    this.fillCalendar()
+    this.fillCalendar();
+    this.months = moment.monthsShort();
+    this.days = moment.weekdaysShort();
+    this.activeYear = Number(moment().format('YYYY'));
+    this.activeMonth = Number(moment().format('M')) - 1;
+    this.years = range(this.activeYear-10, this.activeYear+10);
   }
 
-  componentDidRender() {
-    this.shadowDaysNotAvailable()
+  @Watch('content')
+  contentWatcher(){
+    if(this.content && this.content.data && this.content.data.items) {
+      this.calendarEvents = this.content.data.items;
+    }
   }
+
 
   render() {
     return [
@@ -142,57 +105,34 @@ export class NovaCalendar {
           {/* Barra que va arriba del calendario */}
           {/* De los años */}
           <select onChange={this.nowSetYear.bind(this)}>
-            <option>2009</option>
-            <option>2010</option>
-            <option>2011</option>
-            <option>2012</option>
-            <option>2013</option>
-            <option>2014</option>
-            <option>2015</option>
-            <option>2016</option>
-            <option>2017</option>
-            <option>2018</option>
-            <option>2019</option>
-            <option>2020</option>
+          { this.years.map( year => <option>{year}</option> )}
           </select>
           {/* De los meses */}
           <select onChange={this.nowSetMonth.bind(this)}>
-            <option>January</option>
-            <option>February</option>
-            <option>March</option>
-            <option>April</option>
-            <option>May</option>
-            <option>June</option>
-            <option>July</option>
-            <option>August</option>
-            <option>September</option>
-            <option>October</option>
-            <option>November</option>
-            <option>December</option>
+            { this.months.map( (month, index) => <option selected={this.activeMonth == index + 1} value={index + 1}>{month}</option> )}
           </select>
 
           {/* Para cambiar meses/años */}
-          <button onClick={() => this.nowPrevMonth()} class="calendar__controls__months">Month</button>
+          <button class="calendar__controls__months">Month</button>
           <button onClick={() => this.nowNextMonth()} class="calendar__controls__years">Year</button>
         </div>
         <div class="calendar">
-        <div class="calendar__week calendar__header">
-          <div class="calendar__day">Su</div>
-          <div class="calendar__day">Mo</div>
-          <div class="calendar__day">Tu</div>
-          <div class="calendar__day">We</div>
-          <div class="calendar__day">Th</div>
-          <div class="calendar__day">Fr</div>
-          <div class="calendar__day">Sa</div>
-        </div>
+          <div class="calendar__week calendar__header">
+            { this.days.map( dayName => <div class="calendar__day">{dayName}</div> )}
+          </div>
         {/* El wrapper del calendario */}
-        {this.calendar.map((row)=>
+        {this.calendar.map( row =>
         <div class="calendar__week">
           {row.map( cell =>
-            <div class="calendar__day">
+            <div 
+              class={`calendar__day ${this.activeMonth != cell.month ?  'inactive': ''}`}
+              onClick={ _ => this.nowChangeMonth(cell.month)}>
              <div class="calendar__number">
-              {this.cellContent(cell)}
-             </div> 
+               {cell.day}
+             </div>
+             <ul>
+               {`${this.activeYear}-${cell.month}-${cell.day}`}
+             </ul>
             </div>
           )}
         </div>
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -6,3 +6,7 @@ export function format(first: string, middle: string, last: string): string {
     (last ? ` ${last}` : '')
   );
 }
+
+export function range(start, end) {
+  return Array(end - start + 1).fill(0).map((_, idx) => start + idx)
+}
